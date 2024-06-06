77e927065b4b473d356226760766c204fd87600a
dgstephenson <dgstephenson.econ@gmail.com>
2024-06-06 14:57:35 -0400

    message post request

--- index.js
+++ index.js
@@ -70,40 +70,71 @@ const dateString = getDateString()
 // Message API for Prolific
 // https://docs.prolific.com/docs/api-docs/public/#tag/Messages/operation/SendMessage
 
-const postData = JSON.stringify({
-  recipient_id: '6650ce878485cd00aa153bd6',
-  body: 'This is a test',
-  study_id: '66574c8610e94d979f32aa80'
-})
-const authorization = 'Token c-5FPy0ltEryNu_38lkMHZi-DVnC7odOgCtk379xD_lEKNT3z6r8Sb8njqGoJ4Em3oM_UW1zA7zSck1zLGisM6KbW4NrJjgY90J2Oso68aAzDQQFQxi_FCJp'
-const options = {
-  hostname: 'api.prolific.com',
-  path: '/api/v1/studies/',
-  method: 'GET',
-  headers: {
-    Authorization: authorization
-  },
-  query: {
-    state: 'ACTIVE'
+const prolificAuthorization = 'Token c-5FPy0ltEryNu_38lkMHZi-DVnC7odOgCtk379xD_lEKNT3z6r8Sb8njqGoJ4Em3oM_UW1zA7zSck1zLGisM6KbW4NrJjgY90J2Oso68aAzDQQFQxi_FCJp'
+
+function logStudies () {
+  const options = {
+    hostname: 'api.prolific.com',
+    path: '/api/v1/studies/',
+    method: 'GET',
+    headers: {
+      Authorization: prolificAuthorization
+    },
+    query: {
+      state: 'ACTIVE'
+    }
   }
+  const request = https.request(options, response => {
+    console.log(`statusCode: ${response.statusCode}`)
+    let data = ''
+    response.on('data', chunk => {
+      data += chunk
+    })
+    response.on('end', () => {
+      const studies = JSON.parse(data).results
+      console.log('studies:')
+      studies.forEach(study => {
+        console.log(study.id, study.status)
+      })
+    })
+  })
+  request.on('error', (error) => {
+    console.error(error)
+  })
+  request.end()
 }
-const request = https.request(options, response => {
-  console.log(`statusCode: ${response.statusCode}`)
-  console.log('headers:', response.headers)
-  let data = ''
-  response.on('data', chunk => {
-    data += chunk
+
+function sendMesssage (recipient, message) {
+  const postData = JSON.stringify({
+    recipient_id: recipient,
+    body: message,
+    study_id: '66574c8610e94d979f32aa80'
   })
-  response.on('end', () => {
-    const results = JSON.parse(data).results
-    console.log('results', results)
+  const options = {
+    hostname: 'api.prolific.com',
+    path: '/api/v1/messages/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length,
+      Authorization: prolificAuthorization
+    },
+    query: {
+      state: 'ACTIVE'
+    }
+  }
+  const request = https.request(options, response => {
+    console.log(`statusCode: ${response.statusCode}`)
   })
-})
-request.on('error', (error) => {
-  console.error(error)
-})
-// req.write(postData)
-request.end()
+  request.on('error', (error) => {
+    console.error(error)
+  })
+  request.write(postData)
+  request.end()
+}
+
+logStudies()
+sendMesssage('6650ce123adb3cef7f74e354', 'Another Test')
 
 createDataFile()
 createPaymentFile()

1e6496d32ffd2645909e34c7293102413c6b4518
dgstephenson <dgstephenson.econ@gmail.com>
2024-06-06 14:32:36 -0400

    working get request

--- index.js
+++ index.js
@@ -72,28 +72,38 @@ const dateString = getDateString()
 
 const postData = JSON.stringify({
   recipient_id: '6650ce878485cd00aa153bd6',
-  body: 'Thanks for participating in my study',
+  body: 'This is a test',
   study_id: '66574c8610e94d979f32aa80'
 })
+const authorization = 'Token c-5FPy0ltEryNu_38lkMHZi-DVnC7odOgCtk379xD_lEKNT3z6r8Sb8njqGoJ4Em3oM_UW1zA7zSck1zLGisM6KbW4NrJjgY90J2Oso68aAzDQQFQxi_FCJp'
 const options = {
   hostname: 'api.prolific.com',
   path: '/api/v1/studies/',
   method: 'GET',
   headers: {
-    Authorization: 'c-5FPy0ltEryNu_38lkMHZi-DVnC7odOgCtk379xD_lEKNT3z6r8Sb8njqGoJ4Em3oM_UW1zA7zSck1zLGisM6KbW4NrJjgY90J2Oso68aAzDQQFQxi_FCJp'
+    Authorization: authorization
   },
-  query: 'ACTIVE'
+  query: {
+    state: 'ACTIVE'
+  }
 }
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log('headers:', res.headers)
-  console.log('results:', typeof (res))
+const request = https.request(options, response => {
+  console.log(`statusCode: ${response.statusCode}`)
+  console.log('headers:', response.headers)
+  let data = ''
+  response.on('data', chunk => {
+    data += chunk
+  })
+  response.on('end', () => {
+    const results = JSON.parse(data).results
+    console.log('results', results)
+  })
 })
-req.on('error', (error) => {
+request.on('error', (error) => {
   console.error(error)
 })
 // req.write(postData)
-req.end()
+request.end()
 
 createDataFile()
 createPaymentFile()

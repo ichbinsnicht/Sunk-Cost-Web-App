17475a874f47a5976be583e49b132dbeb4b245cd
Manuel Hoffmann <manuel.hoffmann@ymail.com>
2025-05-02 17:35:43 -0400

    Rounds set to 20 for 100 subject experiment in Prolific

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ export class Game {
     this.scribe = this.server.scribe
     this.numSubjects = 0
     this.subjects = {}
-    this.numPeriods = 3 // 25 periods, numPeriods > numPracticePeriods (internal: 1)
+    this.numPeriods = 20 // 25 periods, numPeriods > numPracticePeriods (internal: 1)
     this.computerlength = 3
     this.choiceLength = 3 // 30 secs choice1 (lab: 15, internal: 3)
     this.feedbackLength = 6 // 15 secs choice1 (lab: 5, internal: 3)
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TO-DO NOW - experimental design adjustments for salient penalty of forcing and revealing forceDir:
 // interface: (CONTINUE HERE!)
-// - update payoffs, outcome feedback, update data
 //
 // - strengthen negative attachment to forcing (resentment of being forced):
 // --> reveal forcedDir in every period
